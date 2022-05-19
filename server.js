caee60e212c189db998e9d3c4f828cc7d5670dd9
JQUAN CALEK <jquancollins96@gmail.com>
2022-05-19 16:16:52 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const app = express();
+const port =3000;
+
+const pokemon = require("./models/pokemon")
+console.log(pokemon)
+
+
+
+// index route
+
+
+
+
+
+
+//show route
+
+
+
+
+
+//new route
+
+
+
+
+
+//edit route
+
+
+
+
+
+//create route
+
+
+
+
+
+//update route
+
+
+
+
+//destroy route
+
+
+
+
+
+//tell the app to listen
+app.listen(port,() => {
+     console.log(`listening on the port`, port)
+ });
+
+
+
+
+
