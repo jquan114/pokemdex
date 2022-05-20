9c03fae16f42bc78f8527f5094bc8e61afd4ed3c
JQUAN CALEK <jquancollins96@gmail.com>
2022-05-20 06:44:46 -0400

    trying to get server working/added some forms and notes on placing the routes, also added css

--- server.js
+++ server.js
@@ -1,57 +1,63 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-const methodOverride = require("method-override");
+// ==========require express,ejs servers, define variables and routes ==================
+const express = require('express')
+const app = express()
+const port = 3000
+const methodOverride = require("method-override")
+// let  pokemon=reqiure('./models/pokemon')
 
-const pokemon = require('./models/pokemon')
+// const morgan = require('morgan')
+// const pokemon = require('./models/pokemon')
 
+//====================middle ware=================
 app.use(express.urlencoded({ extended: false }))
 app.use(methodOverride("_method"))
 // app.use(morgan('dev'))
 app.use("/public", express.static("public"));
 
+//================route intialized============
+
 app.get("/", (req, res) => {
     res.render("index.ejs", {pokemon:pokemon})
-})
-
-//show route
-app.get("/show", (req, res) => {
+});
+//=================show route================
+app.get("/pokemon", (req, res) => {
     res.send(pokemon)
-})
-//new route
+});
+//=================new route=================
 app.get("/new", (req, res) => {
     res.render("new.ejs")
-})
-//edit route
+});
+//=================edit route==================
 app.get("/:id/edit", (req, res) => {
     const { stats } = pokemon[req.params.id]
     res.render("edit.ejs", {stats, id: req.params.id})
-})
-//index route
+});
+//=================index route========================
 app.get("/:index", (req, res) => {
     const { id, name, img, type, stats, moves, damages, misc } = pokemon[parseInt(req.params.index)-1];
     res.render("show.ejs", {id, name, img, type, stats, index:req.params.index})
-})
-//delete route
+});
+//==================delete route=========================
 app.delete("/:index", (req, res) => {
     pokemon.splice(req.params.index, 1)
     res.redirect('/')
-})
+});
 
-//pose route
+//====================post route=======================
 app.post("/", (req, res) => {
-    const { id, name, img, type, hp, attack, defense, spattack, spdefense, speed } = req.body
-    let stats = {hp, attack, defense, spattack, spdefense, speed}
+    const { id, name, img, type,attack, defense, spattack, spdefense, speed } = req.body
+    let stats = { attack, defense, spattack, spdefense, speed}
     let newPokemon = {id, name, img, type, stats}
     pokemon.push(newPokemon)
     res.redirect("/")
-})
-// app put
+});
+//===================index============================
 app.put("/:index", (req, res) => {
-    pokemon[req.params.index].stats = req.body
+    const {attack, defense, spattack, spdefense, speed} = req.body
+    pokemon[req.params.index-1].stats = {attack, defense, spattack, spdefense, speed}
     res.redirect(`/${req.params.index}`)
 })
-//listen for poor 3000
+//==============port listenining======================
 app.listen(port, () => {
     console.log("Listening on port: " + port)
-})
+});
